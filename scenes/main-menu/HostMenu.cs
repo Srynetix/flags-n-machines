507e9b25a1e19c5318b267ad4dec3345b733cd24
Denis BOURGE <denis.bourge@sharingcloud.com>
2021-06-21 08:06:46 +0200

    Menus

---
+++ scenes/main-menu/HostMenu.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+public class HostMenu : ColorRect
+{
+    private IconTouchButton _BackButton;
+
+    public override void _Ready()
+    {
+        _BackButton = GetNode<IconTouchButton>("MainMargin/MainRows/TopColumn/BackButton");
+        _BackButton.Connect(nameof(IconTouchButton.pressed), this, nameof(_GoBack));
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what == MainLoop.NotificationWmGoBackRequest) {
+            _GoBack();
+        }
+    }
+
+    private void _GoBack() {
+        GetTree().ChangeScene("res://scenes/main-menu/MainMenu.tscn");
+    }
+}
--- scenes/main-menu/MainMenu.cs
+++ scenes/main-menu/MainMenu.cs
@@ -3,11 +3,17 @@ using Godot;
 public class MainMenu : ColorRect
 {
     private IconTouchButton _SettingsButton;
+    private Button _JoinButton;
+    private Button _HostButton;
 
     public override void _Ready()
     {
+        _JoinButton = GetNode<Button>("MainMargin/MainRows/MiddleColumn/JoinButton");
+        _HostButton = GetNode<Button>("MainMargin/MainRows/MiddleColumn/HostButton");
         _SettingsButton = GetNode<IconTouchButton>("MainMargin/MainRows/TopColumn/PlayerSection/SettingsButton");
         _SettingsButton.Connect(nameof(IconTouchButton.pressed), this, nameof(_OpenSettingsMenu));
+        _JoinButton.Connect("pressed", this, nameof(_OpenJoinMenu));
+        _HostButton.Connect("pressed", this, nameof(_OpenHostMenu));
 
         // Enable go back on quit
         GetTree().SetQuitOnGoBack(true);
@@ -16,5 +22,15 @@ public class MainMenu : ColorRect
     private void _OpenSettingsMenu() {
         GetTree().SetQuitOnGoBack(false);
         GetTree().ChangeScene("res://scenes/main-menu/SettingsMenu.tscn");
+    }
+
+    private void _OpenJoinMenu() {
+        GetTree().SetQuitOnGoBack(false);
+        GetTree().ChangeScene("res://scenes/main-menu/JoinMenu.tscn");
+    }
+
+    private void _OpenHostMenu() {
+        GetTree().SetQuitOnGoBack(false);
+        GetTree().ChangeScene("res://scenes/main-menu/HostMenu.tscn");
     }
 }
